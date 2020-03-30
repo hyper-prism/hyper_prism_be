ab278a8c782c6ddd331f8c9d5124d6a24d065f08
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-03-29 21:43:42 -0500

    finished router for users

---
+++ routers/userStats.js
@@ -0,0 +1,4 @@
+const router = require('express').Router()
+const userStats = require('../helpers/usersStats-model')
+
+
---
+++ routers/usersRouter.js
@@ -0,0 +1,53 @@
+const router = require('express').Router()
+const Users = require('../helpers/users-model')
+const UserStats = require('../helpers/userStats-model')
+
+
+
+router.get('/', (req, res) => {
+    Users.find().then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        res.status(500).json({error: "there was an error retrieving your info", error})
+    })
+})
+
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+    Users.findById(id).then(response => {
+        if(resonse){
+            res.status(200).json(response)
+        } else {
+            res.status(404).json({error: "Incorrect Credentials"})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({error: "There was an error retrieving your information", error})
+    })
+})
+
+router.post('/', (req, res) => {
+    let body = req.body
+    Users.add(body).then(response => {
+        if(response){
+            res.status(200).json(response)
+        } else {
+            res.status(400).json({error: "Your credentials did not match", error})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({error: "There was an error retrieving your information", error})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params
+    Users.del(id).then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        res.status(500).json({error: "there was an error deleting the user", error})
+    })
+})
