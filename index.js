afd9bf892eaf0abcc374ca3350db69691be2ae7c
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-03-29 20:29:06 -0500

    adjusted index.js so that the port is dynamic based on status

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let server = require('./server/server.js')
 
-let PORT = 8000
+let PORT = process.env.PORT || 8000
 
 server.listen(PORT, () => {
     console.log("Your are connected on port: ", PORT)
